0e9429f9813ecc3dde0a1e5fcc62adf6e902bd35
rgonsahn <rgonsahn@yahoo.com>
2022-08-10 00:31:02 -0400

    created api key on open weather and made var in js for API key

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 //two APIs I need to fetch- first current weather API gets latitude and longitude of city searched get lat and long 
 // place into second API called one call
+var myApiKey= "079955fb255c3ebb2910b2b976633cea";

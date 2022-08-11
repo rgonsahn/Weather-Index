d58cc6c81f0f398b193c075601e2f8e01879712e
rgonsahn <rgonsahn@yahoo.com>
2022-08-11 00:20:43 -0400

    added city search form and header to html using bootstrap

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
 //two APIs I need to fetch- first current weather API gets latitude and longitude of city searched get lat and long 
 // place into second API called one call
 var myApiKey= "079955fb255c3ebb2910b2b976633cea";
-var citySearchEl=$(`#input1`);
+var citySearchEl=$(`#citySearch`);
 var searchBtnEl=$(`#searchBtn`);
 var clearBtn=$('#clearBtn'); 
 
 var presentCity; 
 
-function pullWeather(data){
-    var requestUrl= `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={myApiKey}`
-    fetch(requestUrl)
-    .then(function(response) {
-        return response.json();
-    }
-    )
-}
-
-function getApi() {
-    // replace `octocat` with anyone else's GitHub username
-    var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${myApiKey}`
+function dailyWeather() {
+    var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely,alerts&units=imperial&appid=${myApiKey}`
   
     fetch(requestUrl)
       .then(function (response) {
@@ -26,5 +16,11 @@ function getApi() {
       })
       .then(function (data) {
         console.log(data)
+
+        for (var i = 1; i < 6; i++) {
+            console.log(data.daily[i])
+            // add to html
+        }
       });
   }
+dailyWeather()

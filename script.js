a37b66691e0c498b49a9017d23a801e3ba600e43
rgonsahn <rgonsahn@yahoo.com>
2022-08-22 10:44:05 -0400

    state and city name displays

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ var UvEl = document.getElementById("UV-index")
 var windEl = document.getElementById("wind-speed")
 var historyEl = document.getElementById("SearchHistory")
 var FiveDayHeaderEl = document.getElementById("5DayForecastHeader")
-var CurrentWeatherEl =$("#CurrentDaysWeather")
+var CurrentWeatherEl = $("#CurrentDaysWeather")
+var CurrentWeatherConditions = document.getElementById("CurrentConditions")
+var State = document.getElementById("stateName")
 // var LocalStorageHistory= JSON.parse(localStorage.getItem("search")) || [];
 // var presentCity; 
 
@@ -59,20 +61,22 @@ function GetLatLon(city) {
       console.log(response);
       return response.json();
     })
-  .then(function(data){
-    console.log(data)
-    const HtwoEl= document.createElement("h2")
-    dailyWeather(data[0].lat, data[0].lon) 
-   HtwoEl.textContent= data[0].name
-   FiveDayHeaderEl.appendChild(HtwoEl) 
-   var currentDate = new Date(response.data.dt[0] * 1000)
-   var day= currentDate.getDate() 
-   var month= currentDate.getMonth() + 1 
-   var year= currentDate.getFullYear(); 
-   cityNameEl.textContent= response.data.name + " (" + month + "/" + day + "/" + year + ") ";
-  
-    // inner text grabs data from API
-  })
+    .then(function (data) {
+      console.log(data)
+      const HtwoEl = document.createElement("h2")
+      HtwoEl.textContent = data[0].name
+      cityNameEl.appendChild(HtwoEl)
+      dailyWeather(data[0].lat, data[0].lon)
+      const CurrentState = document.createElement("p")
+      CurrentState.textContent = data[0].state
+      State.appendChild(CurrentState) 
+      const currentDate = document.createElement("p")
+      currentDate.textContent = data.current.dt
+      CurrentWeatherConditions.appendChild(currentDate)
+
+
+      // inner text grabs data from API
+    })
 
 
 }
@@ -107,25 +111,25 @@ function GetLatLon(city) {
 
 
 
-function dailyWeather(lat,lon) {
+function dailyWeather(lat, lon) {
   var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,alerts&units=imperial&appid=${myApiKey}`
 
   fetch(requestUrl)
     .then(function (response) {
+      console.log(response);
       return response.json();
     })
     .then(function (data) {
-      console.log(data) 
-      
+      console.log(data)
 
 
     })
 }
 // dailyWeather() 
-searchBtnEl.addEventListener('click', function(event){
-  var cityName= citySearchEl.val().trim()
+searchBtnEl.addEventListener('click', function (event) {
+  var cityName = citySearchEl.val().trim()
   GetLatLon(cityName)
- 
+
 });
 
 
